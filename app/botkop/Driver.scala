1e8ed2edf9cf22fb8bd9bfc8ea30d3a5ead0b4b0
Koen Dejonghe <koen@fietsoverland.com>
2017-10-22 22:47:48 +0200

    serializing cifar-10

--- app/botkop/Driver.scala
+++ app/botkop/Driver.scala
@@ -8,6 +8,7 @@ import botkop.nn.costs._
 import botkop.nn.data.{Evaluator, MiniBatcher}
 import botkop.nn.gates._
 import botkop.nn.optimizers.{Adam, Nesterov}
+import botkop.numsca.Tensor
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -19,23 +20,29 @@ class Driver extends Actor with Timers with ActorLogging {
 
   implicit val system: ActorSystem = context.system
 
-  // def optimizer = Adam(learningRate = 0.001)
-  def optimizer = Nesterov(learningRate = 0.1)
+  def optimizer = Adam(learningRate = 0.001)
+  // def optimizer = Nesterov(learningRate = 0.1)
 
   val template: Network = ((Linear + Relu) * 2)
-  .withDimensions(784, 50, 10)
-    // .withDimensions(32 * 32 * 3, 50, 10)
+  // .withDimensions(784, 50, 10)
+    .withDimensions(32 * 32 * 3, 50, 10)
     .withOptimizer(optimizer)
     .withCostFunction(softmaxCost)
-    .withRegularization(1e-5)
+  //.withRegularization(1e-5)
 
-  // val trainingDataLoader = new Cifar10DataLoader("data/cifar-10/train", 8)
-  // val devDataLoader = new Cifar10DataLoader("data/cifar-10/test", 1024)
-  // val trainEvalDataLoader = new Cifar10DataLoader("data/cifar-10/train", 1024)
+  val trainingDataLoader =
+    new Cifar10DataLoader(mode = "train", 8)
+  val devEvalDataLoader =
+    new Cifar10DataLoader(mode = "dev", 256)
+  val trainEvalDataLoader =
+    new Cifar10DataLoader(mode = "train", 256, take = Some(2048))
 
-  val trainingDataLoader = new MnistDataLoader("data/mnist/mnist_train.csv.gz", 16)
-  val devEvalDataLoader = new MnistDataLoader("data/mnist/mnist_test.csv.gz", 256)
-  val trainEvalDataLoader = new MnistDataLoader("data/mnist/mnist_train.csv.gz", 256, take=Some(2048))
+  // val trainingDataLoader =
+  // new MnistDataLoader("data/mnist/mnist_train.csv.gz", 16)
+  // val devEvalDataLoader =
+  // new MnistDataLoader("data/mnist/mnist_test.csv.gz", 256)
+  // val trainEvalDataLoader =
+  // new MnistDataLoader("data/mnist/mnist_train.csv.gz", 256, take = Some(2048))
 
   timers.startPeriodicTimer(PersistTick, PersistTick, 30 seconds)
 
@@ -43,6 +50,7 @@ class Driver extends Actor with Timers with ActorLogging {
 
   def empty: Receive = {
     case Start =>
+      log.debug("starting...")
       val nn = template.build
 
       val miniBatcher =
--- app/botkop/data/Cifar10DataLoader.scala
+++ app/botkop/data/Cifar10DataLoader.scala
@@ -1,69 +1,76 @@
 package botkop.data
 
 import java.awt.image.BufferedImage
-import java.io.File
+import java.io._
 import javax.imageio.ImageIO
 
+import botkop.numsca
 import botkop.numsca.Tensor
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.language.postfixOps
 import scala.util.Random
 
-class Cifar10DataLoader(folder: String,
+class Cifar10DataLoader(mode: String,
                         miniBatchSize: Int,
                         take: Option[Int] = None,
                         seed: Long = 231)
     extends DataLoader
     with LazyLogging {
 
-  val labels = List(
-    "airplane",
-    "automobile",
-    "bird",
-    "cat",
-    "deer",
-    "dog",
-    "frog",
-    "horse",
-    "ship",
-    "truck"
-  )
+  import Cifar10DataLoader._
 
-  val n: Int = 32 * 32 * 3
+  val folder: String =
+    if (mode == "train") "data/cifar-10/train.yx" else "data/cifar-10/dev.yx"
 
-  val fileList: List[(Float, File)] = getListOfFiles(folder)
+  val files: List[File] = new File(folder).listFiles
+    .filter(_.isFile)
+    .toList
 
-  override val numSamples: Int = fileList.length
+  override val numSamples: Int = files.length
   override val numBatches: Int =
     (numSamples / miniBatchSize) +
       (if (numSamples % miniBatchSize == 0) 0 else 1)
 
   override def iterator: Iterator[(Tensor, Tensor)] =
     new Random(seed)
-      .shuffle(fileList)
+      .shuffle(files)
       .take(take.getOrElse(numSamples))
       .sliding(miniBatchSize, miniBatchSize)
       .map { sampleFiles =>
-        val xData = sampleFiles map (_._2) flatMap readFile toArray
+        val yxs = sampleFiles map deserialize
 
-        /*
-        val xData = sampleFiles.foldLeft(List.empty[Float]) {
-          case (xs, f) =>
-            val x = readFile(f._2).toList
-            x ::: xs
-        } toArray
-         */
-
-        val yData = sampleFiles map (_._1) toArray
+        val xData = yxs flatMap (_.x) toArray
+        val yData = yxs map (_.y) toArray
 
         val batchSize = sampleFiles.length
 
-        (Tensor(xData).reshape(batchSize, n).transpose,
-         Tensor(yData).reshape(batchSize, 1).transpose)
-      }
+        val x = Tensor(xData).reshape(batchSize, numFeatures)
+        val y = Tensor(yData).reshape(batchSize, 1)
 
-  def readFile(file: File): Seq[Float] = {
+        (x.transpose, y.transpose)
+      }
+}
+
+@SerialVersionUID(123L)
+case class YX(y: Float, x: Array[Float]) extends Serializable
+
+object Cifar10DataLoader extends LazyLogging {
+
+  val numFeatures: Int = 32 * 32 * 3
+
+  def computeMeanImage(files: List[File]): Tensor = {
+    val mean = numsca.zeros(1, numFeatures)
+
+    files.foreach { f =>
+      val lof = readImage(f)
+      val t = Tensor(lof.toArray).reshape(1, numFeatures)
+      mean += t
+    }
+    mean / files.length
+  }
+
+  def readImage(file: File): Seq[Float] = {
     val image: BufferedImage = ImageIO.read(file)
 
     val w = image.getWidth
@@ -85,6 +92,20 @@ class Cifar10DataLoader(folder: String,
   }
 
   def getListOfFiles(dir: String): List[(Float, File)] = {
+
+    val labels = List(
+      "airplane",
+      "automobile",
+      "bird",
+      "cat",
+      "deer",
+      "dog",
+      "frog",
+      "horse",
+      "ship",
+      "truck"
+    )
+
     val d = new File(dir)
     d.listFiles
       .filter(_.isFile)
@@ -99,6 +120,37 @@ class Cifar10DataLoader(folder: String,
         case (i, cat, file) =>
           (labels.indexOf(cat).toFloat, file)
       }
+  }
+
+  def serializeDataset(outputFolder: String,
+                       fileList: List[(Float, File)],
+                       meanImage: Tensor): Unit =
+    fileList.foreach {
+      case (yData, file) =>
+        val xData = readImage(file).toArray
+        val x = Tensor(xData).reshape(1, numFeatures) - meanImage
+
+        val oos = new ObjectOutputStream(
+          new FileOutputStream(s"$outputFolder/${file.getName}.yx"))
+        oos.writeObject(YX(yData, x.data.map(_.toFloat)))
+        oos.close()
+    }
+
+  def deserialize(file: File): YX = {
+    val ois = new ObjectInputStream(new FileInputStream(file))
+    val yx = ois.readObject.asInstanceOf[YX]
+    ois.close()
+    yx
+  }
+
+  def main(args: Array[String]): Unit = {
+    val trainingFileList = getListOfFiles("data/cifar-10/train")
+    logger.debug("caculating mean image from training set")
+    val meanImage = computeMeanImage(trainingFileList.map(_._2))
+    logger.debug("serializing training set")
+    serializeDataset("data/cifar-10/train.yx", trainingFileList, meanImage)
+    logger.debug("serializing dev set")
+    serializeDataset("data/cifar-10/dev.yx", trainingFileList, meanImage)
   }
 
 }

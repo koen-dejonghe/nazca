db4073cafd9b464045f8556d5a7cc04cd2969c54
Koen Dejonghe <koen@fietsoverland.com>
2017-12-07 06:09:47 +0100

    extracting numsca

--- app/botkop/nn/costs/Softmax.scala
+++ app/botkop/nn/costs/Softmax.scala
@@ -21,29 +21,4 @@ case object Softmax extends Cost {
     (loss, dx)
   }
 
-  /*
-  override def costFunction(xt: Tensor, yt: Tensor): (Double, Tensor) = {
-
-    val x = xt.T
-    val y = yt.T
-
-    val shiftedLogits = x - ns.max(x, axis = 1)
-    val z = ns.sum(ns.exp(shiftedLogits), axis = 1)
-    val logProbs = shiftedLogits - ns.log(z)
-    val probs = ns.exp(logProbs)
-    val n = x.shape(0)
-
-    println(logProbs.shape.toList)
-    println(y.shape.toList)
-
-    val loss = -ns.sum(logProbs(ns.arange(n).T, y)) / n
-
-    val dx = probs
-    // dx.put(y, _ - 1)
-    dx.put(_ - 1)(ns.arange(n), y)
-    dx /= n
-
-    (loss, dx.T)
-  }
-  */
 }
--- build.sbt
+++ build.sbt
@@ -22,7 +22,10 @@ libraryDependencies += "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Tes
 // libraryDependencies += "com.typesafe.akka" %% "akka-remote" % akkaVersion
 
 // libraryDependencies += "org.nd4j" % "nd4j-native-platform" % "0.8.0"
-libraryDependencies += "org.nd4j" % "nd4j-native-platform" % "0.9.1"
+// libraryDependencies += "org.nd4j" % "nd4j-native-platform" % "0.9.1"
+
+
+libraryDependencies += "be.botkop" %% "numsca" % "0.1.0"
 
 libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
